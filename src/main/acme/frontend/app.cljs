663731d0d704e71435350a8c9d25a13a7e5de6f0
Alan Thompson <cloojure@gmail.com>
2021-01-29 11:00:15 -0800

    misc

--- src/main/acme/frontend/app.cljs
+++ src/main/acme/frontend/app.cljs
@@ -1,25 +1,32 @@
-(ns acme.frontend.app)
-(js/console.log "#app02")
+(ns acme.frontend.app
+  (:require ["voca" :as voca])) ;use npm lib after `npm install voca`
 
 (enable-console-print!)
 
+(js/console.log "#app02")
 
 ; these are not called for the initial load, only subsequent loads
 (defn ^:dev/before-load before []
-  (println :before-load)
-  ) ; same as (println ...)
+  (println :before-load))     ; same as (println ...)
+
 (defn ^:dev/after-load after []
-  (println :after-all)
-  ) ; same as (js/console.log ...)
+  (println :after-all))     ; same as (js/console.log ...)
+
+(defn sample []
+  (set! js/newVar 3)
+  (println :js/newVar js/newVar)
+  (println :voca (voca/camelCase "foo bar"))
+  )
 
 (defn ^:export init [] ; shadow will call upon initial load only
   (js/console.log "init - enter again")
   (println "Hello World")
+  (sample)
   (set! (.. js/document (getElementById "app-target") -innerHTML) "Hello, Document!")
   (js/console.log "init - leave")
   )
 
-(js/console.log "#app12"  )
+(js/console.log "#app12")
 ; (init)
 (js/console.log "#app14")
 
