1cb397866a421ee369a839fc7f6627426a1b5d98
Alan Thompson <cloojure@gmail.com>
2021-01-28 20:34:48 -0800

    got printing to console

--- src/main/acme/frontend/app.cljs
+++ src/main/acme/frontend/app.cljs
@@ -1,4 +1,15 @@
 (ns acme.frontend.app)
+(js/console.log "#app02")
 
-(defn init []
-  (println "Hello World"))
+(enable-console-print!)
+
+(defn ^:export init []
+  (js/console.log "init - enter")
+  (println "Hello World")
+  (js/console.log "init - leave")
+  "Hello Again"
+  )
+
+(js/console.log "#app12")
+(init)
+(js/console.log "#app14")
